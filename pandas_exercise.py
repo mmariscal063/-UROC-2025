d3786bd6bc9ed0fe0f0d376a55c3670c31222a70
mmariscal063 <mmariscal063@gmail.com>
2025-06-03 16:17:53 -0700

    Add files via upload
    This is my original work.

---
+++ pandas_exercise.py
@@ -0,0 +1,19 @@
+#FOR LOOP WITH RANGE, same as your note book just diff formatting, heavy emmphasis on the FOR statement
+print("For Loop excercise, counting 1 to 10:")
+for i in range(1,10): #creates loop that runs 10 times,
+    print(f"Count: {i}")
+
+print("\n" + "="*40 + "\n") #this just creates a border
+
+#FOR LOOP WITH A LIST
+print ("For Loop excercise with a list")
+fruits = ["maracuya", "garambullo", "pitaya", "tuna", "chichituna"] #list of strings
+print("My favorite fuits:")
+for fruit in fruits: # loops through each item in the fruits list, calling each item 'fruit'
+    print(" I like " + fruit) #prints each fruit name in a sentence
+print("\n" + "★"*40 + "\n")
+
+# what is the correct way to declare a Python variable?
+# answer: x = 5 (created a varibale with a numeric value of 5)
+
+
